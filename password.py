ff903317ceb537a092118a07da74683af2c6e1b6
Jeff Hsiao <samy88188@gmail.com>
2024-07-10 19:07:00 +0800

    while time > 0

--- password.py
+++ password.py
@@ -1,15 +1,11 @@
 pwd = 'a123456'
 time = 3
 
-while True:
-	if time > 0:
-		input_pass = input('請輸入密碼: ')
-		if input_pass == pwd:
-			print('登入成功!!!')
-			break
-		else:
-			time -= 1
-			if time >0:
-				print('密碼錯誤! 還有', time,'次機會')			
-	else:
+while time > 0:	
+	input_pass = input('請輸入密碼: ')
+	if input_pass == pwd:
+		print('登入成功!!!')
 		break
+	else:
+		time -= 1			
+		print('密碼錯誤! 還有', time,'次機會')
